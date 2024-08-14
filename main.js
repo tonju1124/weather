7c07fc862f6faba69669ac11a38f55db8c2343ba
tonju <tonju112420@gmail.com>
2024-08-14 17:22:12 +0800

    uncommented

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 import "./index.css";
 
-// const apiKey = import.meta.env.VITE_API_KEY
-// fetch(`https://api.openweathermap.org/data/2.5/weather?q=Banting&appid=${apiKey}&unit=metric`)
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log(data);
-//     })
-//     .catch(error => {
-//         console.error('Error fetching the weather data:', error);
-//     });
+const apiKey = import.meta.env.VITE_API_KEY
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=Banting&appid=${apiKey}&unit=metric`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('Error fetching the weather data:', error);
+    });
 
 const weatherImg = document.querySelector('.weather-img img.weather-icon');
 
