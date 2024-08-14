05661fcb31f4822916ee252cf2cae9ecec076e0d
tonju <tonju112420@gmail.com>
2024-08-14 17:21:24 +0800

    feat::added icon to the waetherbox

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 import "./index.css";
 
-const apiKey = import.meta.env.VITE_API_KEY
+// const apiKey = import.meta.env.VITE_API_KEY
+// fetch(`https://api.openweathermap.org/data/2.5/weather?q=Banting&appid=${apiKey}&unit=metric`)
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data);
+//     })
+//     .catch(error => {
+//         console.error('Error fetching the weather data:', error);
+//     });
 
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=Banting&appid=${apiKey}&unit=metric`)
-    .then(response => response.json())
-    .then(data => { 
-        console.log(data);
-    })
-    .catch(error => {
-        console.error('Error fetching the weather data:', error);
-    });
+const weatherImg = document.querySelector('.weather-img img.weather-icon');
+
+weatherImg.src = 'svg/sun-w-clouds.svg';
+weatherImg.alt = 'Sun with clouds';
